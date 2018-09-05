bf8064cf7dcbaf654fe36540b235d6084a54f539
LILIT <lilit555ai@gmail.com>
2018-09-05 13:49:37 -0600

    reate python file

---
+++ hw1_regression.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+
+lambda_input = int(sys.argv[1])
+sigma2_input = float(sys.argv[2])
+X_train = np.genfromtxt(sys.argv[3], delimiter = ",")
+y_train = np.genfromtxt(sys.argv[4])
+X_test = np.genfromtxt(sys.argv[5], delimiter = ",")
+
